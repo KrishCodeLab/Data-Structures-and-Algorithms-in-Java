e6cec8c2ea3801c23d498c14b1dd69273e1e8d08
Krushna Govardhane <krushnagoverdhane@gmail.con>
2026-08-31 21:34:10 +0530

    Learned Java variables and data types

---
+++ JavaBasic_Variables_Datatypes_.java
@@ -0,0 +1,41 @@
+public class JavaBasic_Variables_Datatypes_ {
+  public static void main(String[] args) {
+
+    // Print Function in java
+    System.out.println("Hello Krishna !!!");
+    System.out.println("Let's Go Again !!!");
+    System.out.println("Everything is God's Plan !!!");
+    System.out.print("krishna All the best");
+    System.out.print("/n You will be the Java Developer ");
+
+    // print pattern
+    System.out.println("*");
+    System.out.println("**");
+    System.out.println("***");
+    System.out.println("****");
+    System.out.println("*****");
+
+    // java is typed Language there is need to declared datatype
+    // Python,C++ is also typed languages
+
+    // Variable Declaration
+    int a = 1;
+    int b = 2;
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(a + b);
+
+    // Data Types :
+    // Primitive Data Type :char, Byte,Short,int,float,boolean,long,double
+    int number = 121; // 4 byte
+    String str = "Krishna"; // 2 byte
+    byte bit = 127; // 1 byte
+    short number2 = 540; // 2 byte
+    float number3 = 9; // 4 bytte
+    double number4 = 99.99999999999; // 8 byte
+    long number5 = 12849933; // 8 byte
+
+    // Non Primitive Data Type : Array,String,object,Class,Interface
+
+  }
+}
