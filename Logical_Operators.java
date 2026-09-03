cbdf4b5b201c4022af200a576903f6a6138dd316
Krushna Govardhane <krushnagoverdhane@gmail.com>
2026-09-03 09:10:36 +0530

    Logical Operator

---
+++ Logical_Operators.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+import javax.swing.text.StyledEditorKit;
+
+public class Logical_Operators {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    // Logical Operators :- and,or
+    System.out.println("Enter Makrs");
+    int marks = sc.nextInt();
+
+    // Grade
+    if (marks >= 90 & marks <= 100) {
+      System.err.println("Grade A+ ");
+    } else if (marks >= 80 & marks <= 89) {
+      System.out.println("Grade A");
+    } else if (marks >= 70 & marks <= 79) {
+      System.out.println("Grade B");
+    } else if (marks >= 60 & marks <= 69) {
+      System.out.println("Grade C");
+    } else if (marks >= 101) {
+      System.out.println("WRONG INFORMARION !!! ");
+    } else {
+      System.out.println("Avg");
+    }
+
+    sc.close();
+  }
+}
