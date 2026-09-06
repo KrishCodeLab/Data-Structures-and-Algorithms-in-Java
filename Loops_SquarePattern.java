723467184f2ea6e49efd2f35b91fcdcaaa2a1124
Krushna Govardhane <krushnagoverdhane@gmail.com>
2026-09-06 19:55:33 +0530

    Loop Question Square Pattern

---
+++ Loops_SquarePattern.java
@@ -0,0 +1,19 @@
+public class Loops_SquarePattern {
+  public static void main(String[] args) {
+    // print square pattern
+    int count = 0;
+
+    while (count <= 4) {
+      System.out.println("* * * * ");
+      count++;
+    }
+    System.out.println();
+    System.out.println();
+    System.out.println();
+
+    for (int i = 0; i <= 4; i++) {
+      System.out.println("* * * * ");
+
+    }
+  }
+}
