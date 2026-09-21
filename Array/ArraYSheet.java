29428abae83106f469eeab32ad9db9db990d720d
Krushna Govardhane <krushnagoverdhane@gmail.com>
2026-09-21 21:50:09 +0530

    DSA Question : Binary Search

---
+++ Array/ArraYSheet.java
@@ -0,0 +1,52 @@
+// // package Array;
+
+// // public class ArraYSheet {
+
+// // }
+
+// LEVEL 1 — Basic Traversal
+// ↓
+// Print
+// Sum
+// Count
+// Average
+// Search
+// ↓
+// LEVEL 2 — Min / Max
+// ↓
+// Maximum
+// Minimum
+// Second Largest
+// Second Smallest
+// ↓
+// LEVEL 3 — Two Pointers
+// ↓
+// Reverse
+// Palindrome
+// Swap
+// Sorted check
+// ↓
+// LEVEL 4 — Manipulation
+// ↓
+// Move zeros
+// Rotate
+// Remove duplicates
+// ↓
+// LEVEL 5 — Nested Loops
+// ↓
+// Pairs
+// Duplicates
+// Triplets
+// ↓
+// LEVEL 6 — Hashing
+// ↓
+// Frequency
+// Two Sum
+// Duplicates
+// ↓
+// LEVEL 7 — Advanced
+// ↓
+// Prefix Sum
+// Sliding Window
+// Kadane
+// Binary Search
---
+++ Array/Array_BinarySearch.java
@@ -0,0 +1,30 @@
+package Array;
+
+import java.util.*;
+
+public class Array_BinarySearch {
+  public static void BinarySearch(int n[], int x) {
+    int start = 1;
+    int end = n.length - 1;
+    while (start <= end) {
+      int mid = (start + end) / 2;
+      if (n[mid] == x) {
+        System.out.println("Found at idx " + mid);
+        break;
+      }
+
+      if (n[mid] < x) {
+        mid = mid + 1;
+      } else {
+        mid = mid - 1;
+      }
+    }
+
+  }
+
+  public static void main(String[] args) {
+
+    int n[] = { 2, 4, 6, 8, 12, 14, 16, 18, 20 };
+    BinarySearch(n, 12);
+  }
+}
